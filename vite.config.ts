95135d51993c18f6cd07f6da0b477b33cdb8beea
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-04-30 19:18:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig, searchForWorkspaceRoot } from "vite";
+import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		fs: {
-			allow: [searchForWorkspaceRoot(process.cwd())]
-		}
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
