f8abaa1bf368baa40c691022d8ec2f019107b859
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-06-07 11:37:32 +0100

    Update auth.sh

--- auth.sh
+++ auth.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 PROFILE=( $(cat .aws_profile) )
-aws codeartifact --region eu-central-1 --profile $PROFILE login --tool npm --repository versetools --domain packages
+aws codeartifact --region eu-central-1 --profile $PROFILE login --tool npm --repository versetools --domain packages --namespace @versetools
