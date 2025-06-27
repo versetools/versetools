05b16156532d1908b60a9f1cedb6f741b83614f2
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-06-27 17:44:02 +0100

    Update auth.sh

--- auth.sh
+++ auth.sh
@@ -2,3 +2,4 @@
 
 PROFILE=( $(cat .aws_profile) )
 aws codeartifact --region eu-central-1 --profile $PROFILE login --tool npm --repository versetools --domain packages --namespace @versetools
+aws codeartifact --region eu-central-1 --profile $PROFILE login --tool npm --repository l3dev-private --domain packages --namespace @l3dev-private
