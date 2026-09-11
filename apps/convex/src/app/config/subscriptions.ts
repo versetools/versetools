8e2f1f5599ba2ec36da94ff816bf93a231122957
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2026-09-11 20:19:40 +0100

    Update subscriptions.ts

--- apps/convex/src/app/config/subscriptions.ts
+++ apps/convex/src/app/config/subscriptions.ts
@@ -1,10 +1,10 @@
-import { subscriptionRegistryId } from "@versetools/core/config/ids/commands";
+import { genericSubscriptionRegistryId } from "@versetools/core/config/ids/commands";
 import { SubscriptionRegistry } from "@versetools/core/services/commands/subscriptions/SubscriptionRegistry";
 import { bind, optimisticSingletonScope } from "haywire";
 
 import type { DataModel } from "$convex/_generated/dataModel";
 
-export const subscriptionRegistryBinding = bind(subscriptionRegistryId)
+export const subscriptionRegistryBinding = bind(genericSubscriptionRegistryId)
 	.withGenerator(() => {
 		const registry = new SubscriptionRegistry<DataModel>();
 
