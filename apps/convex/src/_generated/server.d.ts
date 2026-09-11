13fac0bd20019467d43529549a91825b910f8e03
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2026-09-11 20:26:20 +0100

    Update generated files

--- apps/convex/src/_generated/server.d.ts
+++ apps/convex/src/_generated/server.d.ts
@@ -22,6 +22,27 @@ import {
 import type { DataModel } from "./dataModel.js";
 
 /**
+ * Typesafe environment variables.
+ *
+ * This includes platform-provided env vars and any variables declared in
+ * `convex.config.ts`.
+ */
+type Env = {
+  readonly CONVEX_CLOUD_URL: string;
+  readonly CONVEX_SITE_URL: string;
+  readonly AWS_ACCESS_KEY_ID: string;
+  readonly AWS_SECRET_ACCESS_KEY: string;
+  readonly CONVEX_SECRET: string;
+  readonly POSTHOG_FLAGS_POLLING_INTERVAL_SECONDS: string | undefined;
+  readonly POSTHOG_HOST: string | undefined;
+  readonly POSTHOG_PERSONAL_API_KEY: string | undefined;
+  readonly POSTHOG_PROJECT_TOKEN: string;
+  readonly SITE_URL: string;
+  readonly UPLOADTHING_API_KEY: string;
+  readonly UPLOADTHING_APP_ID: string;
+};
+
+/**
  * Define a query in this Convex app's public API.
  *
  * This function will be allowed to read your Convex database and will be accessible from the client.
@@ -94,6 +115,14 @@ export declare const internalAction: ActionBuilder<DataModel, "internal">;
  * @returns The wrapped function. Import this function from `convex/http.js` and route it to hook it up.
  */
 export declare const httpAction: HttpActionBuilder;
+
+/**
+ * Typesafe environment variables.
+ *
+ * This includes platform-provided env vars and any variables declared in
+ * `convex.config.ts`.
+ */
+export declare const env: Env;
 
 /**
  * A set of services for use within Convex query functions.
